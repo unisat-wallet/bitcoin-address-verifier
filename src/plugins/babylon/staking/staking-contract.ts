21328a85aafe9ba2fe1344d3b883763e44eae47d
Govard Barkhatov <gbarkhatov@gmail.com>
2025-05-27 10:46:34 +0300

    internal pubkey

--- src/plugins/babylon/staking/staking-contract.ts
+++ src/plugins/babylon/staking/staking-contract.ts
@@ -2,15 +2,13 @@ import { payments, initEccLib } from "bitcoinjs-lib";
 import { StakingScriptData } from "@babylonlabs-io/btc-staking-ts";
 import { Taptree } from "bitcoinjs-lib/src/types";
 import * as ecc from "@bitcoin-js/tiny-secp256k1-asmjs";
+
 import { ContractNetwork } from "../../../core-sdk/types";
 import { networkToBitcoinNetwork } from "../../../core-sdk/utils";
+import { internalPubkey } from "../utils/internalPubKey";
 
 initEccLib(ecc);
 
-// internalPubkey denotes an unspendable internal public key to be used for the taproot output
-const key =
-  "0250929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0";
-const internalPubkey = Buffer.from(key, "hex").subarray(1, 33); // Do a subarray(1, 33) to get the public coordinate
 export function getStakingContract(
   params: {
     stakerPk: string;
