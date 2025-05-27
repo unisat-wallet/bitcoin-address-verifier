116d3468467d70c3e0b16c5128a1177587fa4b37
Govard Barkhatov <gbarkhatov@gmail.com>
2025-05-27 14:02:51 +0300

    rm unused jest setup parts

--- src/jest.setup.js
+++ src/jest.setup.js
@@ -1,22 +1,3 @@
 const { initBTCCurve } = require("./plugins/babylon/utils/btc");
 
-// const originalTest = global.test;
-// const NUM_ITERATIONS = 10;
-
 initBTCCurve();
-
-// global.test = (name, fn, timeout) => {
-//   for (let i = 0; i < NUM_ITERATIONS; i++) {
-//     originalTest(`${name} (iteration ${i + 1})`, fn, timeout);
-//   }
-// };
-
-// const originalIt = global.it;
-
-// global.it = (name, fn, timeout) => {
-//   for (let i = 0; i < NUM_ITERATIONS; i++) {
-//     originalIt(`${name} (iteration ${i + 1})`, fn, timeout);
-//   }
-// };
-
-// global.it.each = originalIt.each;
