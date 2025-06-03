02f9236975fb645b49d64b7773f5a601f25579d3
slient-coder <slient-coder@unisat.io>
2025-06-03 17:23:19 +0800

    v0.2.2

--- src/generated/plugins.auto.ts
+++ src/generated/plugins.auto.ts
@@ -1,13 +1,15 @@
-// Auto-generated at 2025-06-03T07:05:34.891Z
+// Auto-generated at 2025-06-03T09:22:32.564Z
 // DO NOT EDIT DIRECTLY
 
 import BabylonSlashing from '../plugins/babylon/slashing';
+import BabylonSlashingBurn from '../plugins/babylon/slashing-burn';
 import BabylonStaking from '../plugins/babylon/staking';
 import BabylonUnbonding from '../plugins/babylon/unbonding';
 import Brc20DevModule from '../plugins/brc20-dev/module';
 
 export function registerAllPlugins(registry) {
   registry.register(BabylonSlashing);
+  registry.register(BabylonSlashingBurn);
   registry.register(BabylonStaking);
   registry.register(BabylonUnbonding);
   registry.register(Brc20DevModule);
